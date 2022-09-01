d93b9fd9b380e524f7b886cbe1c588e742421ee4
giusscos <giu.cosenza08@gmail.com>
2022-09-01 15:51:34 +0200

    Aggiunta generatore di email dinamico

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('ciao mondo!')
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Vue - Gen Emails',
+        emailsNum: '',
+        emailRandom: new Array (),
+    },
+    methods: {
+        genEmail(emails){
+            for(let i = 0; i < emails; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((res) => {
+                            this.emailRandom.push(res.data.response)
+                            // console.log(this.emailRandom)
+                    })
+            }
+            this.emailsNum = ''
+        }
+    },
+})
